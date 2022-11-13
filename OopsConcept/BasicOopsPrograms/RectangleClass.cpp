46b932c5208ae3c1ff5b6a4d0cb36d03230fbff8
Ibrahim-786 <ibrahimpasha112@gmail.com>
2022-11-13 13:03:11 +0530

    ..

---
+++ OopsConcept/BasicOopsPrograms/RectangleClass.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+
+
+
+class Rectangle
+{
+
+	public:
+		int length;
+		int breath;
+
+	int area()
+	{
+		return length*breath;
+	}
+
+	int perimeter()
+	{
+
+		return 2*(length*breath);
+	}
+
+};
+
+
+int main(int argc, char *argv[], char *envs[])
+{
+
+	Rectangle R1, R2;
+
+
+	R1.length = 10;
+	R1.breath = 20;
+
+	std::cout<<"Area is : "<<R1.area();
+
+	std::cout<<"\n"<<"Perimeter is : "<<R1.perimeter();
+
+	return 0;
+}
+
